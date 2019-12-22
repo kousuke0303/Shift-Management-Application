f29f5acb718584465cdf8d463037783014480fd1
EC2 Default User <ec2-user@ip-172-31-42-167.ap-northeast-1.compute.internal>
2019-12-22 01:00:39 +0000

    ユーザーの能力カラム追加

--- db/migrate/20191126114300_create_users.rb
+++ db/migrate/20191126114300_create_users.rb
@@ -3,7 +3,9 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :staff_type
+      t.boolean :leader
+      t.boolean :kitchen
+      t.boolean :hole
       t.boolean :admin, default: false
       t.string :remember_digest
 
