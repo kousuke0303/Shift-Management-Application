531b42d556a0fa5d3fb443b287c5309003f58e18
EC2 Default User <ec2-user@ip-172-31-38-130.ap-northeast-1.compute.internal>
2020-01-19 03:24:18 +0000

    退勤押下時に給与計算してsalaryカラムに日給を保存

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -14,16 +14,16 @@ class Attendance < ApplicationRecord
   #     self.salary = day_work_time * User.find(current_user.id).hourly_wage
   #   end
   # end
-  
+  # 日給の計算を行う
   before_save :calculate_salary_per_day
   
   def calculate_salary_per_day
+    # 退勤時間が存在時(退勤押下時)に日給の計算を行う
     if self.work_end_time.present?
       # 実労働時間の計算
       day_work_time = format("%.2f", ((self.work_end_time.floor_to(15.minutes) - (self.break_end_time - self.break_start_time) - self.work_start_time.floor_to(15.minutes)) / 60) / 60.0)
       # 日給の計算(実労働時間*そのスタッフの時給)
       self.salary = day_work_time.to_f * User.find(self.user_id).hourly_wage
-      debugger
     end
   end
 end
