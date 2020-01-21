65bd7fcae6ba5af919d2c607219ac97566be09b9
EC2 Default User <ec2-user@ip-172-31-42-167.ap-northeast-1.compute.internal>
2020-01-21 02:41:39 +0000

    過去のシフト確認機能完成

--- app/helpers/shifts_helper.rb
+++ app/helpers/shifts_helper.rb
@@ -101,4 +101,12 @@ module ShiftsHelper
     @first_day.day <= 15 ?
     "#{@first_day.year}-#{@first_day.month}-16".to_date : @first_day.next_month.beginning_of_month
   end
+  
+  # 現在の最新のシフトか確認
+  def current_shifts?
+    Date.current.day <= 15 ?
+    current_first_day = Date.current.beginning_of_month :
+    current_first_day = "#{Date.current.year}-#{Date.current.month}-16".to_date
+    return true unless @first_day == current_first_day
+  end
 end
