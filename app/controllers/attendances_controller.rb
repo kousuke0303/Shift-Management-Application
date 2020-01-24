1e0c54a1e45ce19af3974785c6acea68ce0d1c68
EC2 Default User <ec2-user@ip-172-31-38-130.ap-northeast-1.compute.internal>
2020-01-24 23:41:43 +0000

    管理者ログイン時のみ出退勤登録画面に移動する

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,23 +2,27 @@ class AttendancesController < ApplicationController
   before_action :logged_in_user
   
   def register
-    @attendances = Attendance.where(day: Date.today).where.not(work_start_time: nil)
-    # 今日出勤済みかどうか調べる
-    if Attendance.find_by(user_id: current_user.id, day: Date.today)
-      # 出勤済みでなければ自分のユーザIDの今日日付のレコードを抽出
-      @attendance = Attendance.where(user_id: current_user.id).where(day: Date.today)
+    # 管理者でログインした場合、出退勤登録画面に移動する
+    if current_user.admin
+      # 今日出勤済みかどうか調べる
+      if Attendance.find_by(user_id: current_user.id, day: Date.current)
+        # 出勤済みでなければ自分のユーザIDの今日日付のレコードを抽出
+        @attendances = Attendance.where(user_id: current_user.id).where(day: Date.current)
+      end
+    else
+      # 従業員でログインした場合、シフト確認画面に移動する
+      redirect_to shifts_current_shifts_user_path(current_user.id)
     end
   end
   
   def create
     # 今日出勤済みかどうか調べる
-    if Attendance.find_by(user_id: current_user.id, day: Date.today)
+    if Attendance.find_by(user_id: current_user.id, day: Date.current)
       flash[:info] = "今日はもう出勤済みです。"
       redirect_to users_attendances_register_path(current_user)
     else
       # 出勤ボタン押下時にレコードが生成される
-      # 日本時間に合わせる為、9時間分の秒数を足す
-      @attendance = Attendance.new(day: Date.today, user_id: current_user.id, work_start_time: Time.current.change(sec: 0) + 32400)
+      @attendance = Attendance.new(day: Date.current, user_id: current_user.id, work_start_time: Time.current.change(sec: 0))
       if @attendance.save
         flash[:info] = "おはようございます！"
       else
@@ -30,10 +34,10 @@ class AttendancesController < ApplicationController
   
   def update
     # 退勤ボタン押下時、その日のレコードのwork_end_timeをupdateする
-    @attendance = Attendance.where(user_id: current_user.id).where(day: Date.today)
+    @attendance = Attendance.where(user_id: current_user.id).where(day: Date.current)
     # 出勤時間が未登録であることを判定します。
     if @attendance[0].work_end_time.nil?
-      if @attendance[0].update_attributes(work_end_time: Time.current.change(sec: 0) + 32400)
+      if @attendance[0].update_attributes(work_end_time: Time.current.change(sec: 0))
         flash[:info] = "お疲れ様でした！"
       else
         flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
@@ -44,10 +48,10 @@ class AttendancesController < ApplicationController
   
   def breakstart
     # 休憩開始ボタン押下時、その日のレコードのbreak_start_timeに現在時刻を挿入する
-    @attendance = Attendance.where(user_id: current_user.id).where(day: Date.today)
+    @attendance = Attendance.where(user_id: current_user.id).where(day: Date.current)
     # 休憩開始時間が未登録であることを判定します。
     if @attendance[0].break_start_time.nil?
-      if @attendance[0].update_attributes(break_start_time: Time.current.change(sec: 0) + 32400)
+      if @attendance[0].update_attributes(break_start_time: Time.current.change(sec: 0))
         flash[:info] = "休憩を開始しました。"
       else
         flash[:danger] = "休憩開始に失敗しました。やり直してください。"
@@ -58,10 +62,10 @@ class AttendancesController < ApplicationController
   
   def breakend
     # 休憩終了ボタン押下時、その日のレコードのbreak_end_timeに現在時刻を挿入する
-    @attendance = Attendance.where(user_id: current_user.id).where(day: Date.today)
+    @attendance = Attendance.where(user_id: current_user.id).where(day: Date.current)
     # 休憩開始時間が未登録であることを判定します。
     if @attendance[0].break_end_time.nil?
-      if @attendance[0].update_attributes(break_end_time: Time.current.change(sec: 0) + 32400)
+      if @attendance[0].update_attributes(break_end_time: Time.current.change(sec: 0))
         flash[:info] = "休憩を終了しました。"
       else
         flash[:danger] = "休憩終了に失敗しました。やり直してください。"
