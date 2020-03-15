8efb8e888bc8a59301f8a7964f3fed12761342b2
EC2 Default User <ec2-user@ip-172-31-42-167.ap-northeast-1.compute.internal>
2020-03-15 02:37:34 +0000

    コンフリ解消

--- config/routes.rb
+++ config/routes.rb
@@ -24,13 +24,10 @@ Rails.application.routes.draw do
       get 'edit_user_info'
       patch 'update_user_info'
       get 'shifts/current_shifts'
-<<<<<<< HEAD
       get 'shifts/next_shifts'
       patch 'shifts/change_release_status'
-=======
       get 'new_password_reset'
       patch 'update_password_reset'
->>>>>>> develop
     end
     collection do
       get 'new_password_reset_index'
