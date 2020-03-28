23e38546be52ea4084455848bf0533af8a80aac8
EC2 Default User <ec2-user@ip-172-31-32-79.ap-northeast-1.compute.internal>
2020-03-28 09:47:31 +0000

    20200328_本番環境_ユーザー更新実装完了_スタッフ一覧の並べ替え実装完了_スタッフ検索/年月検索エラー解消中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,13 +30,13 @@ class ApplicationController < ActionController::Base
     #月が10月以降の時と、10月以前で年月日検索を条件分岐
     if @attendance.present? && @attendance.day.to_date.month >= 10
       @attendances = Attendance
-                     .where(user_id: params[:user_id])
+                     .where("user_id IN (?)", params[:user_id])
                      .where("day LIKE ?", "#{params['day(1i)']}" << "-" + "#{params['day(2i)']}%")
                      .or(Attendance.where(day: params[:day]))
                      .order("day")
     else
       @attendances = Attendance
-                     .where(user_id: params[:user_id])
+                     .where("user_id IN (?)", params[:user_id])
                      .where("day LIKE ?", "#{params['day(1i)']}" << "-0" + "#{params['day(2i)']}%")
                      .or(Attendance.where(day: params[:day]))
                      .order("day")
