e2f35afd575bc8fd051b9d3fc00978ce91478805
EC2 Default User <ec2-user@ip-172-31-32-79.ap-northeast-1.compute.internal>
2020-03-30 11:38:04 +0000

    20200330_「ログインを保持する」チェック時、再度ブラウザを開き直した時のリダイレクト先を変更_2回目

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
       params[:session][:remember_me] == "1" ? remember(user) : forget(user)
-      if !user.admin
-        redirect_to shifts_current_shifts_user_url(user)
-      else
+      if user.admin
+        log_in user
         redirect_to users_attendances_register_url(user)
+      else
+        log_in user
+        redirect_to shifts_current_shifts_user_url(user)
       end
     else
       flash[:danger] = "認証に失敗しました。"
